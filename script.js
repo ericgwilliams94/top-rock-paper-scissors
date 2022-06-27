dbef32e0e7b72c2c6598136eeda2bdd0c0bd5693
Eric Williams <egw.egwilliams@gmail.com>
2022-06-27 17:14:44 -0400

    fixed playRound function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var COMPCHOICE = -1;
+var COMPCHOICE;
 
 function computerPlay () {
     var choice = Math.floor(Math.random() * 3)
@@ -6,48 +6,46 @@ function computerPlay () {
     if (choice == 0) {
         COMPCHOICE = 'rock'
     }
-    else if (choice = 1) {
+    else if (choice == 1) {
         COMPCHOICE = 'paper'
     }
-    else {
+    else if (choice == 2) {
         COMPCHOICE = 'scissors'
     }
 }
 
-computerPlay()
-
-
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
-  return 'Loser'
+  return 'You lose! Scissors beats paper!';
   }
   else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock') {
-  return 'Winner'
+  return 'You win! Paper beats rock!';
   }
   else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
-  return 'Winner'
+  return 'You win! Scissors beats paper!';
   }
   else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock') {
-  return 'Loser'
+  return 'You lose! Rock beats scissors!';
   }
   else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-  return 'Tie'
+  return 'Tie! You and the computer selected the same!';
   }
   else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') {
-  return 'Winner'
+  return 'You win! Rock beats scissors!';
   }
   else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper') {
-  return 'Loser'
+  return 'You lose! Paper beats rock!';
   }
   else {
-  return 'Invalid responses'
+  return 'Invalid responses';
   }
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        playRound('paper', COMPCHOICE)
+        computerPlay();
+        playRound('paper', COMPCHOICE);
     }
 }
 
-console.log(game())
+console.log(game());
