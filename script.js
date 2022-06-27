32bfe7485cf92247ebe667eab201a329d301e1fa
Eric Williams <egw.egwilliams@gmail.com>
2022-06-26 21:10:25 -0400

    added playRound function

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase == 'paper') {
-        return 'You lose! Paper beats rock!';
-    }
-    else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase == 'scissors') {
-        return 'You win! Rock beats paper!';
-    }
-    else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase == 'scissors') {
-        console.log('You lose! Paper beats scissors!');
-    } 
-    else {
-        return 0;
-    }
+	if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
+  return 'Loser'
+  }
+  else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock') {
+  return 'Winner'
+  }
+  else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
+  return 'Winner'
+  }
+  else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock') {
+  return 'Loser'
+  }
+  else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+  return 'Tie'
+  }
+  else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') {
+  return 'Winner'
+  }
+  else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper') {
+  return 'Loser'
+  }
+  else {
+  return 'Invalid responses'
+  }
 }
 
 playRound('paper', 'SCISSORS');
