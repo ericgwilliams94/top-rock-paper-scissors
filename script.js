8a0ed8c07b91a5a179969fbd5970a2248d8757c8
Eric Williams <egw.egwilliams@gmail.com>
2022-07-06 18:36:56 -0400

    revised playRound function

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const scissors = document.getElementById("scissors");
 // variables for functions
 var userScore = 0;
 var compScore = 0;
+var gameMsg;
 
 
 // function for computer selection
@@ -30,72 +31,22 @@ function computerPlay () {
 
 // function to play round
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
-  return 'You lose! Scissors beats paper!';
-  compScore += 1;
-  }
-  else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock') {
-  return 'You win! Paper beats rock!';
-  userScore += 1;
-  }
-  else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
-  return 'You win! Scissors beats paper!';
-  userScore += 1;
-  }
-  else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock') {
-  return 'You lose! Rock beats scissors!';
-  compScore += 1;
-  }
-  else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-  return 'Tie! You and the computer selected the same!';
-  }
-  else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') {
-  return 'You win! Rock beats scissors!';
-  userScore += 1;
-  }
-  else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper') {
-  return 'You lose! Paper beats rock!';
-  compScore += 1;
-  }
-  else {
-  return 'Invalid responses';
-  }
-}
-
-// player select
-rock.onclick = () => alert('ROCK CLICKED');
-paper.onclick = () => alert('PAPER CLICKED');
-scissors.onclick = () => alert('SCISSORS CLICKED');
-
-// function to check for a winner
-function isWinner() {
-    if (userScore == 5  || compScore == 5) {
-        /* 
-        declare winner
-        show play again button
-        */
+	// declare tie
+    if (playerSelection == computerSelection) {
+        message ="Tie";
     }
-}
-
-// play again function
-function anotherOne() {
-    // reset scores
-    userScore, compScore = 0;
-    // update scores on screen
-    // run game script
-}
-
-// play again button
-const btnPlayAgain = document.getElementById("btn-play-again");
-btnPlayAgain.onclick = () => anotherOne();
-
-/* 
-while user score or compuer score not equal to 5
-listen for user selection,
-generate comp selection & play game,
-display result and add score approriately
-check for winner, display winner if met
-*/
-function game() {
-    return 'Waiting for input...';
+    // player wins
+    if (playerSelection == "rock" && computerSelection == "scissors" ||
+    playerSelection == "paper" && computerSelection == "rock" ||
+    playerSelection == "scissors" && computerSelection == "paper") {
+        var message = "You win!";
+        userScore += 1
+    }
+    // computer wins
+    if (playerSelection == "rock" && computerSelection == "paper" ||
+    playerSelection == "paper" && computerSelection == "scissors" ||
+    playerSelection == "scissors" && computerSelection == "rock") {
+        var message = "You lose!";
+        compScore += 1;
+    }
 }
